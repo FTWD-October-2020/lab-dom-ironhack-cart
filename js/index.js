89b8de5ea4aeec65052a1b1212b59b7ec26b2b64
Viviana Garcia <vivianandrea4@gmail.com>
2020-10-23 15:23:51 -0400

    vivi changes

--- js/index.js
+++ js/index.js
@@ -11,22 +11,26 @@
 //     let quantity =  Number(product.querySelector(".quantity input").value);
 //     let subtotal = price * quantity;
 //     product.querySelector(".subtotal span").innerHTML = subtotal;
-    
+
 //   }
 // }
 document.querySelector("#calculate").onclick = calculateAll;
 
 
 
-function calculateAll(event){
+function calculateAll(event) {
   let products = document.querySelectorAll('.product')
 
-
-  for(let product of products){
+  let grandTotal = 0
+  for (let product of products) {
     let price = Number(product.querySelector(".price span").innerText);
-    let quantity =  Number(product.querySelector(".quantity input").value);
+    let quantity = Number(product.querySelector(".quantity input").value);
     let subtotal = price * quantity;
     product.querySelector(".subtotal span").innerHTML = subtotal;
-    
+    grandTotal += subtotal
   }
+  console.log('grandTotal', grandTotal)
+  document.querySelector("#total-value span").innerHTML = grandTotal
 }
+
+
